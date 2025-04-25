86ac6cc1e4520fb73efa10ef2b0a30e975542d7d
Ankit Lingwal <ankitlingwal9@gmail.com>
2025-04-26 01:28:17 +0530

    Alchemy api key and sepolia private key added to env

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config(); // <-- Important
 
-require ("@nomicfoundation/hardhat-toolbox")
+const { SEPOLIA_API_KEY, PRIVATE_KEY } = process.env;
+
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${SEPOLIA_API_KEY}`,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
