850d038b02207b58ce7816bd725fa21eba51948f
Ankit Lingwal <ankitlingwal9@gmail.com>
2025-04-25 23:46:14 +0530

    Mocha framework hooks

--- test/token.js
+++ test/token.js
@@ -2,17 +2,22 @@ const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
 describe("Token", function () {
+    let Token;
+    let hardhatToken;
+    let add1;
+    let add2;
+    let adds;
+    let owner;
+
+    beforeEach(async function () {
+        Token = await ethers.getContractFactory("Token");
+        [owner, add1, add2, ...adds] = await ethers.getSigners();
+        hardhatToken = await Token.deploy();
+    });
+
     it("Deployment should assign the total supply of tokens to the owner", async function () {
-        const [owner] = await ethers.getSigners(); // ✅ FIXED
-        console.log("Signers Object", owner);
-
-        const Token = await ethers.getContractFactory("Token"); 
-        // Looks for a contract named "Token" inside your contracts/ folder. forms connection between the contract and the token.js file 
-        const hardhatToken = await Token.deploy();
-
         const ownerBalance = await hardhatToken.balanceOf(owner.address);
-        console.log("Owner address:", owner.address);
-
-        expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+        const totalSupply = await hardhatToken.totalSupply();
+        expect(ownerBalance).to.equal(totalSupply);
     });
 });
